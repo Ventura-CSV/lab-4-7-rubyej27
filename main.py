727db0e0f4ec8450c98ca81ba0707ba442d1e743
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-06-26 11:24:05 -0400

    4-7 commit

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def main():
     Code Your Program here
     ########################################
     """
+    previous_value = None
+    
+    while True:
+        current_value = int(input('Enter an integer: '))
+        
+        if previous_value is None or current_value <= previous_value:
+            numbers.append(current_value)
+            previous_value = current_value
+        else:
+            break
+    
 
     ########################################
     # Do not delete the return statement
